68bad5b649ebcf5da27fb46b57a21cde25657faf
Sher Bhachu <sherbhachu@shers-mbp-2.home>
2018-01-29 19:06:04 +0000

    Added a basic test to check for number of results returned

--- spec/basic_api_calls_spec.rb
+++ spec/basic_api_calls_spec.rb
@@ -1,11 +1,14 @@
 describe 'Check that the Weather API is serving us weather info' do
 
   describe 'GET requests' do
-    it '200 OK when I request the Weather API endpoint' do
+    it 'Responsds with a 200 OK when I request the /posts endpoint' do
       get('/posts')
       expect_status 200
     end
 
+    it 'Returns more than a single post'
+      get('/posts')
+      expect(json_body.size).to be > 1
   end
 
 end
